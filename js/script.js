ca261e5cbe757ca8e51a7266a2613110942b0feb
MohmmedHasan-99 <mo7ammed.mah@gmail.com>
2021-08-21 18:00:16 +0300

    add sounds effects to th game relates #11

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const gameSound = new Audio("../assets/sounds/ingame.wav");
+const clickSound = new Audio("../assets/sounds/click.wav");
+
+const playBtn = document.getElementById("play");
+const instructionBtn = document.getElementById("instruction");
+
+const playSound = () =>{
+    gameSound.loop = true;
+    gameSound.play();
+}
+playSound();
+
+const play = () =>{
+    clickSound.play();
+}
+  
+playBtn.addEventListener("click", play);
+instructionBtn.addEventListener("click", play);
