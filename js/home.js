3a1e7e00a35b4e139946d2a640439cb9d61740e3
MohmmedHasan-99 <mo7ammed.mah@gmail.com>
2021-08-21 12:56:10 +0300

    gereate computer random move relates #7

--- js/home.js
+++ js/home.js
@@ -6,13 +6,24 @@ const choices = document.querySelectorAll(".choice-img");
 
 const displayRock = () => {
   playerHand.setAttribute("src", "../assets/images/L1.png");
+  generateComputerMove();
 };
 const displayPaper = () => {
   playerHand.setAttribute("src", "../assets/images/L2.png");
+  generateComputerMove();
 };
 
 const displayScissors = () => {
   playerHand.setAttribute("src", "../assets/images/L3.png");
+  generateComputerMove();
+};
+
+const generateComputerMove = () => {
+  let min = Math.ceil(1);
+  let max = Math.floor(3);
+  let random =  Math.floor(Math.random() * (max - min + 1) + min);
+  console.log(random)
+  computerHand.setAttribute("src", `../assets/images/R${random}.png`);
 };
 
 choices[0].addEventListener("click", displayRock);
